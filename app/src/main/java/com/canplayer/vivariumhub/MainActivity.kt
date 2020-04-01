09519486d2364c18bd9a6aafb68bbbc3602f9561
canplayer <chenchi98@live.com>
2020-04-01 17:42:10 +0800

    test

--- app/src/main/java/com/canplayer/vivariumhub/MainActivity.kt
+++ app/src/main/java/com/canplayer/vivariumhub/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.canplayer.vivariumhub
 
-import androidx.appcompat.app.AppCompatActivity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private val REQUEST_ENABLE_BT = 777
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.btn_search).setOnClickListener {
-         Toast.makeText(this,"77777",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "77777", Toast.LENGTH_LONG).show()
+            val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            mBluetoothAdapter = bluetoothManager.adapter
+            if(!mBluetoothAdapter!!.isEnabled){
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
         }
     }
 }
